04f014655aec3772b46a3b3c321b5361ddb2a498
sergeyt <stodyshev@gmail.com>
2013-08-14 10:26:27 +0700

    vm.js stub

---
+++ src/runtime/vm.js
@@ -0,0 +1,17 @@
+function VM(metadata, args) {
+
+	// TODO steps:
+	// goto entry point
+	// parse the body instructions
+	// build AST and compile it to the function
+	// call the compiled function with args
+
+	function resolveToken(token) {
+	}
+
+	// methodEnv object:
+	// body - stream to read method body
+	// 
+	function call(methodEnv) {
+	}
+}
